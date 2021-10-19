9bd17e28d3d47c954f46b2de252f09e283c86ab5
doczi-dominik <doczi.dominik0@gmail.com>
2021-10-19 11:15:30 +0200

    ✨ Added battery script

--- blocks.go
+++ blocks.go
@@ -6,6 +6,7 @@ var (
 	cmdstropt = "-c"        // command string opt for shell
 
 	blocks = []block{
+		{cmd: "$BARSCRIPTS/battery.sh", upInt: 60, inSh: true},
 		{icon: "", cmd: "date +%H:%M", upInt: 60},
 	}
 )
