42951074a3cba39433e2cf3e453366c82da45d7b
doczi-dominik <doczi.dominik0@gmail.com>
2021-10-20 22:23:47 +0200

    ✨ Added display brightness script

--- blocks.go
+++ blocks.go
@@ -6,6 +6,7 @@ var (
 	cmdstropt = "-c"        // command string opt for shell
 
 	blocks = []block{
+		{icon: "", cmd: "$BARSCRIPTS/brightness.sh", upSig: 10, inSh: true},
 		{cmd: "$BARSCRIPTS/battery.sh", upInt: 60, inSh: true},
 		{icon: "", cmd: "date +%H:%M", upInt: 60},
 	}
